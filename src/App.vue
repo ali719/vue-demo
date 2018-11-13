86585162a68fbac80c1cae3816065018742358ff
liujiaxin <2531252999@.qq.com>
2018-11-13 20:41:33 +0800

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
-8iu0700[//<template>
-  <div class="container">
-    <Search/>
-    <UserMain/>
+<template>
+  <div>
+    <p>click {{count}} times count </p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
   </div>
 </template>
 
 <script>
 
-  import Search from './components/Search.vue'
-  import Main from './components/Main.vue'
 export default {
-    components:{
-      Search,
-      UserMain:Main
+  data(){
+    return{
+      count:0
     }
+  },
+
+  methods:{
+    increment(){
+      this.count++
+    },
+    decrement(){
+      this.count--
+    },
+    incrementIfOdd(){
+      if (this.count % 2 === 1){
+        this.count++
+      }
+    },
+    incrementAsync(){
+      setTimeout(() => {
+        this.count++
+      },1000)
+    },
+  }
 }
-
-
 </script>
 
 <style>
